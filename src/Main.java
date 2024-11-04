414e8ecc603190c07e4c9d5670e7326b95ee9508
juhotuominen <juho.m.tuominen@outlook.com>
2024-11-04 12:35:21 +0200

    support websites

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ import opennlp.tools.langdetect.Language;
 import opennlp.tools.langdetect.LanguageDetectorME;
 import opennlp.tools.langdetect.LanguageDetectorModel;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -16,22 +21,15 @@ public class Main {
             // Initialize LanguageDetector
             LanguageDetectorME languageDetector = new LanguageDetectorME(model);
 
-            // Input text for language detection
-            String englishText = "The quick brown fox jumps over the lazy dog. Can you tell me where the nearest restaurant is?"; // English example
-            String frenchText = "Le renard brun rapide saute par-dessus le chien paresseux. Pouvez-vous me dire où se trouve le restaurant le plus proche?";  // French example
-            String finnishText = "Hei, anteeksi! Voitko kertoa minulle, missä on lähin ravintola?";  // Finnish example
-            String koreanText = "빠른 갈색 여우가 게으른 개를 뛰어 넘습니다. 가장 가까운 레스토랑이 어디 있는지 알려줄 수 있나요?"; // Korean example
+            String link = args[0]; // Get link from arguments
 
-            String languages[] = new String [] {englishText, frenchText, finnishText, koreanText};
+            Document doc = Jsoup.connect(link).get();
+            Elements lines = doc.select("p");
+            String linesToString = lines.html();
 
-            for(int i = 0; i < languages.length; i++ )
-            {
-                // Detect the language
-                Language detectedLanguage = languageDetector.predictLanguage(languages[i]);
-                // Print detected language and confidence score
-                System.out.println("Detected Language: " + detectedLanguage.getLang());
-                System.out.println("Confidence: " + detectedLanguage.getConfidence() + "\n");
-            }
+            Language detectedLanguage = languageDetector.predictLanguage(linesToString);
+            System.out.println("\n\nDetected Language: " + detectedLanguage.getLang());
+            System.out.println("Confidence: " + detectedLanguage.getConfidence() + "\n");
 
         } catch (IOException e) {
             e.printStackTrace();
